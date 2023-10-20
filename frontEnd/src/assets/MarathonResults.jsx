2c6d5526ad26e31af1d828ad0eb371ddff510bbf
Graeme Corrigan <corrigangk@gmail.com>
2023-10-20 10:31:02 -0400

    getting files caught up

--- frontEnd/src/assets/MarathonResults.jsx
+++ frontEnd/src/assets/MarathonResults.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-import DisplayResults from "./DisplayResults";
-import CreateForm from "./CreateForm";
-import UpdateResult from "./UpdateResult";
-import DeleteButton from "./DeleteButton";
+import DisplayResults from "./DisplayResults.jsx";
+import CreateForm from "./CreateNewResult.jsx";
+import UpdateResult from "./UpdateResult.jsx";
+import DeleteButton from "./DeleteResult.jsx";
 
 const MarathonResults = () => {
   const [results, setResults] = useState([]);
@@ -12,7 +12,22 @@ const MarathonResults = () => {
     setResults([...results, newResult]);
   };
 
-  const onResultDelete = () => {};
+  const onResultDelete = async(runnerID) => {
+    try {
+      const response = await fetch(`http://localhost:5543/api/results/${runnerID}`, {
+        method: "DELETE",
+      });
+
+      if(response.ok) {
+        const updatedResults = results.filter((result) => result.runnerID !== runnerID);
+        setResults(updatedResults);
+      } else {
+        console.error("Error deleting marathon result");
+      }
+    } catch (error) {
+      console.error("Error deleting marathon result", error);
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
